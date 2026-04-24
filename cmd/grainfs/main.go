6b1297be75a88ac3d695666323e7077498783680
Choe, Cheng-Dae <whitekid@gmail.com>
2026-04-25 01:28:04 +0900

    feat(serve): --log-level flag — slog + hlog 동시 제어
    PersistentPreRunE에서 slog default handler와 Hertz hlog 레벨을
    동시에 설정. 기본값 info; 프로파일링 시 --log-level=warn으로
    로깅 mutex 경합 제거 확인됨.

--- cmd/grainfs/main.go
+++ cmd/grainfs/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,30 @@ var rootCmd = &cobra.Command{
 	Use:     "grainfs",
 	Short:   "GrainFS - Lightweight S3-compatible object storage",
 	Version: version,
+}
+
+func init() {
+	rootCmd.PersistentFlags().String("log-level", "info", "log level: debug, info, warn, error")
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		lvlStr, _ := rootCmd.PersistentFlags().GetString("log-level")
+		var slogLvl slog.Level
+		switch lvlStr {
+		case "debug":
+			slogLvl = slog.LevelDebug
+			hlog.SetLevel(hlog.LevelDebug)
+		case "warn", "warning":
+			slogLvl = slog.LevelWarn
+			hlog.SetLevel(hlog.LevelWarn)
+		case "error":
+			slogLvl = slog.LevelError
+			hlog.SetLevel(hlog.LevelError)
+		default:
+			slogLvl = slog.LevelInfo
+			hlog.SetLevel(hlog.LevelInfo)
+		}
+		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slogLvl})))
+		return nil
+	}
 }
 
 func main() {
