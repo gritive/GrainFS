0ccfbd3b35e3e543b127944b6f7d632d022b673c
Choe, Cheng-Dae <whitekid@gmail.com>
2026-05-19 15:40:58 +0900

    feat(storage/fbs): add Tag table and Object.tags field

--- internal/storage/storagepb/Object.go
+++ internal/storage/storagepb/Object.go
@@ -181,7 +181,7 @@ func (rcv *Object) PartsLength() int {
 	return 0
 }
 
-func (rcv *Object) AppendCallMd5s(obj *BytesValue, j int) bool {
+func (rcv *Object) Tags(obj *Tag, j int) bool {
 	o := flatbuffers.UOffsetT(rcv._tab.Offset(26))
 	if o != 0 {
 		x := rcv._tab.Vector(o)
@@ -193,7 +193,7 @@ func (rcv *Object) AppendCallMd5s(obj *BytesValue, j int) bool {
 	return false
 }
 
-func (rcv *Object) AppendCallMd5sLength() int {
+func (rcv *Object) TagsLength() int {
 	o := flatbuffers.UOffsetT(rcv._tab.Offset(26))
 	if o != 0 {
 		return rcv._tab.VectorLen(o)
@@ -246,10 +246,10 @@ func ObjectAddParts(builder *flatbuffers.Builder, parts flatbuffers.UOffsetT) {
 func ObjectStartPartsVector(builder *flatbuffers.Builder, numElems int) flatbuffers.UOffsetT {
 	return builder.StartVector(4, numElems, 4)
 }
-func ObjectAddAppendCallMd5s(builder *flatbuffers.Builder, appendCallMd5s flatbuffers.UOffsetT) {
-	builder.PrependUOffsetTSlot(11, flatbuffers.UOffsetT(appendCallMd5s), 0)
+func ObjectAddTags(builder *flatbuffers.Builder, tags flatbuffers.UOffsetT) {
+	builder.PrependUOffsetTSlot(11, flatbuffers.UOffsetT(tags), 0)
 }
-func ObjectStartAppendCallMd5sVector(builder *flatbuffers.Builder, numElems int) flatbuffers.UOffsetT {
+func ObjectStartTagsVector(builder *flatbuffers.Builder, numElems int) flatbuffers.UOffsetT {
 	return builder.StartVector(4, numElems, 4)
 }
 func ObjectEnd(builder *flatbuffers.Builder) flatbuffers.UOffsetT {
