9430fcfd1f9524373f577a2117136f2a28eb9be0
Choe, Cheng-Dae <whitekid@gmail.com>
2026-06-13 18:55:43 +0900

    feat(raftpb): add user_metadata to forward PutObjectArgs (make fbs)
    Append a user_metadata:[Tag] field to PutObjectArgs so a forwarded PUT
    can carry the client's x-amz-meta-* headers. Wire-compatible append; old
    peers read it as empty. Reuses the existing Tag{key,value} table.

--- internal/raft/raftpb/PutObjectArgs.go
+++ internal/raft/raftpb/PutObjectArgs.go
@@ -107,8 +107,28 @@ func (rcv *PutObjectArgs) SseAlgorithm() []byte {
 	return nil
 }
 
+func (rcv *PutObjectArgs) UserMetadata(obj *Tag, j int) bool {
+	o := flatbuffers.UOffsetT(rcv._tab.Offset(14))
+	if o != 0 {
+		x := rcv._tab.Vector(o)
+		x += flatbuffers.UOffsetT(j) * 4
+		x = rcv._tab.Indirect(x)
+		obj.Init(rcv._tab.Bytes, x)
+		return true
+	}
+	return false
+}
+
+func (rcv *PutObjectArgs) UserMetadataLength() int {
+	o := flatbuffers.UOffsetT(rcv._tab.Offset(14))
+	if o != 0 {
+		return rcv._tab.VectorLen(o)
+	}
+	return 0
+}
+
 func PutObjectArgsStart(builder *flatbuffers.Builder) {
-	builder.StartObject(5)
+	builder.StartObject(6)
 }
 func PutObjectArgsAddBucket(builder *flatbuffers.Builder, bucket flatbuffers.UOffsetT) {
 	builder.PrependUOffsetTSlot(0, flatbuffers.UOffsetT(bucket), 0)
@@ -127,6 +147,12 @@ func PutObjectArgsStartBodyVector(builder *flatbuffers.Builder, numElems int) fl
 }
 func PutObjectArgsAddSseAlgorithm(builder *flatbuffers.Builder, sseAlgorithm flatbuffers.UOffsetT) {
 	builder.PrependUOffsetTSlot(4, flatbuffers.UOffsetT(sseAlgorithm), 0)
+}
+func PutObjectArgsAddUserMetadata(builder *flatbuffers.Builder, userMetadata flatbuffers.UOffsetT) {
+	builder.PrependUOffsetTSlot(5, flatbuffers.UOffsetT(userMetadata), 0)
+}
+func PutObjectArgsStartUserMetadataVector(builder *flatbuffers.Builder, numElems int) flatbuffers.UOffsetT {
+	return builder.StartVector(4, numElems, 4)
 }
 func PutObjectArgsEnd(builder *flatbuffers.Builder) flatbuffers.UOffsetT {
 	return builder.EndObject()
